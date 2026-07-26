f5e038a7cd446908d7859f5e69e61f782fddd6e8
sahiljadhav7 <jadhavsahil36751@gmail.com>
2026-07-26 23:57:06 +0530

    Initialsing server

---
+++ server/src/server.js
@@ -0,0 +1,75 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import config from "./shared/config/index.js";
+import logger from "./shared/config/logger.js";
+import mongodb from "./shared/config/mongodb.js";
+import postgres from "./shared/config/postgres.js";
+import rabbitmq from "./shared/config/rabbitmq.js";
+import errorHandler from "./shared/middlewares/errorHandler";
+import ResponseFormatter from "./shared/utils/responseFormatter.js";
+import { version } from "mongoose";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.path}`, {
+    ip: req.ip,
+    userAgent: req.headers["user-agent"],
+  });
+  next();
+});
+
+app.get("/health", (req, res) => {
+  req.status(200).json(
+    ResponseFormatter.success(
+      {
+        status: "healthy",
+        timestamp: new Date().toISOString(),
+        uptime: process.uptime(),
+      },
+      "service is healthy",
+    ),
+  );
+});
+
+app.use("/", (req, res) => {
+  res.status(200).json(
+    ResponseFormatter.success(
+      {
+        service: "API Hit Monitoring System",
+        version: "1.0.0",
+        endpoints: {
+          health: "/health",
+          auth: "/api/auth",
+          ingest: "/api/hit",
+          analytics: "/api/analytics",
+        },
+      },
+      "API Hit Monitoring Service",
+    ),
+  );
+});
+
+app.use((req, res) => {
+  res.status(404).json(ResponseFormatter.error("Endpoint not found", 404));
+});
+
+async function initializeConnection() {
+  try {
+    logger.info("Initializing database connections...");
+
+    await mongodb.connect();
+    await postgres.testConnection();
+    await rabbitmq.connect();
+
+    logger.info("All connections established succesfully");
+  } catch (error) {
+    logger.info("Failed to establish connection");
+  }
+}
