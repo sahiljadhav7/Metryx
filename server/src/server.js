3bc2f711887a9d92af3d07c6efe415586dcfe013
sahiljadhav7 <jadhavsahil36751@gmail.com>
2026-07-30 23:48:26 +0530

    Wrote the functionf for shutting down entirely and also wrote the dockerfile'

--- server/src/server.js
+++ server/src/server.js
@@ -84,5 +84,30 @@ async function startServer() {
       logger.info(`Environment: ${config.node_env}`);
       logger.info(`API available at: http://localhost:${config.port}`);
     });
-  } catch (error) {}
+
+    const gracefulShutdonw = async (singel) => {
+      logger.info(`${signal} recieved, shutting down gracefully`);
+      server.close(async () => {
+        logger.info("HTTP server closed");
+        try {
+          await mongodb.disconnect();
+          await postgres.close();
+          await rabbitmq.close();
+          logger.info("All connecitons closed, exiting process");
+          process.exit(0);
+        } catch (error) {
+          logger.error("Error during shutdown", error);
+          process.exit(1);
+        }
+      });
+
+      setTimeout(() => {
+        logger.error("Forced shutdown");
+        process.exit(1);
+      }, 1000);
+    };
+  } catch (error) {
+    logger.error("Failed to start server:", error);
+    process.exit(1);
+  }
 }
